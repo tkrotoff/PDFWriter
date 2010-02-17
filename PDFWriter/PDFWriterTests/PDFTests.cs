028c920d58aba0eef9a9fef89fdcd97cde191b3e
Tanguy Krotoff <tkrotoff@gmail.com>
2010-02-17 12:44:49 +0000

    Add unit tests

---
+++ PDFWriter/PDFWriterTests/PDFTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace PDFWriterTests
+{
+    [TestFixture]
+    class PDFTests
+    {
+        [Test]
+        public void Test1PagePDF()
+        {
+        }
+
+        [Test]
+        public void Test2PagesPDF()
+        {
+        }
+
+        [Test]
+        public void Test3PagesPDF()
+        {
+        }
+    }
+}
